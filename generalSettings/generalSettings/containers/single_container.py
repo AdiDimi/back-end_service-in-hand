798685f519ba6922f7e13c124a3c7d5c9f4c59b3
AdiDimi <adidimi1973@gmail.com>
2023-06-05 14:15:41 +0300

    with Dockerfiles

--- generalSettings/generalSettings/containers/single_container.py
+++
@@ -1,8 +0,0 @@
-from dependency_injector import containers, providers
-from requestTypes.repository.requestsType_repository import requestTypesRepository
-
-class requestTypesContainer(containers.DeclarativeContainer):
-    requestTypesService =  providers.Singleton(requestTypesRepository)
-
-# class RecipeAppContainer(containers.DeclarativeContainer):
-#         requesttypescontainer = providers.Container(requestTypesContainer)
